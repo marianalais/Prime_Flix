22b612f2d8052e0024fe9721f09a437ca6b1e27f
marianalais <124471672+marianalais@users.noreply.github.com>
2023-03-10 14:42:04 -0300

    filmes carregados

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,48 @@
+import { useEffect, useState} from 'react';
+import api from '../../services/api';
+import { Link } from 'react-router-dom';
+import './home.css';
+
+// URL DA API: /movie/now_playing?api_key=28fc232cc001c31e8a031f419d0a14ca&language=pt-BR
 
 function Home(){
+  const [filmes, setFilmes] = useState([]);
+
+
+  useEffect(()=>{
+
+    async function loadFilmes(){
+      const response = await api.get("movie/now_playing", {
+        params:{
+         api_key: "0185abf6c16ce98f8770e64155b1eb1b",
+         language: "pt-BR",
+         page: 1,
+        }
+      })
+
+      //console.log(response.data.results.slice(0, 10));
+      setFilmes(response.data.results.slice(0, 10));
+
+    }
+
+    loadFilmes();
+
+  }, [])
+
   return(
-    <div>
-      <h1>BEM VINDO A HOME</h1>
+    <div className="container">
+      <div className="lista-filmes">
+        {filmes.map((filme) => {
+          return(
+            <article key={filme.id}>
+              <strong> {filme.title} </strong>
+              <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+              <Link to={`/filme/${filme.id}`}>Acessar</Link>
+            </article>
+          )
+        })}
+      </div>
+     
     </div>
   )
 }
